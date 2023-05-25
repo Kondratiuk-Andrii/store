a766353eb5e601d932ec1e1b19d700f3ff9601fd
Kondratiuk-Andrii <andrii.kondratiuk13@gmail.com>
2023-05-25 20:02:30 +0200

    Added manipulating into admin

--- products/admin.py
+++ products/admin.py
@@ -1,14 +1,24 @@
 from django.contrib import admin
 
-from products.models import Product, ProductCategory
-
-# Register your models here.
-
-admin.site.register(Product)
+from products.models import Product, ProductCategory, Basket
 
 
+@admin.register(ProductCategory)
 class ProductCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'slug')
     prepopulated_fields = {'slug': ('name',)}
 
 
-admin.site.register(ProductCategory, ProductCategoryAdmin)
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'quantity', 'category')
+    fields = ('name', 'description', ('price', 'quantity'), 'image', 'category')
+    search_fields = ('name', 'category__name')
+    ordering = ('name',)
+
+
+class BasketAdmin(admin.TabularInline):
+    model = Basket
+    fields = ('product', 'quantity', 'created_timestamp')
+    readonly_fields = ('created_timestamp',)
+    extra = 0
